2cb8b3cd82bfff1abca71a93851aa92aef143b1c
MysticJ <37102399+MysticJ@users.noreply.github.com>
2020-12-27 22:32:31 +0100

    Add files via upload

---
+++ audiotrans.m
@@ -0,0 +1,138 @@
+% % % % %
+% Wireless Receivers: algorithms and architectures
+% Audio Transmission Framework 
+%
+%
+%   3 operating modes:
+%   - 'matlab' : generic MATLAB audio routines (unreliable under Linux)
+%   - 'native' : OS native audio system
+%       - ALSA audio tools, most Linux distrubtions
+%       - builtin WAV tools on Windows 
+%   - 'bypass' : no audio transmission, takes txsignal as received signal
+
+% Configuration Values
+conf.audiosystem        = 'matlab';     % Values: 'matlab','native','bypass'
+conf.bitsps             = 16;           % bits per audio sample\
+conf.nframes            = 1;
+conf.modulation_order   = 2;            % BPSK:1, QPSK:2
+conf.npreamble          = 100;          % length of preamble
+
+conf.f_spacing          = 5;
+conf.nsubcarriers       = 300;
+if mod(conf.nsubcarriers,2) ~= 0
+   disp('WARNING: Number of subcarriers should be even.'); 
+end
+conf.f_sampling         = 48000; 
+conf.os_factor          = conf.f_sampling / conf.nsubcarriers / conf.f_spacing;
+assert(mod(conf.os_factor, 1) == 0, 'over-sampling factor should be integer');
+conf.f_mixer            = 1000;
+conf.nbits              = 10000;    % number of bits ? per frame?
+conf.nsyms              = ceil(conf.nbits/conf.modulation_order);
+conf.cp_factor          = 0.5;
+conf.lenOFDMsym_cp      = ceil(conf.nsubcarriers*conf.os_factor*(1+conf.cp_factor));
+conf.nOFDMsyms          = ceil(conf.nsyms/conf.nsubcarriers);
+
+% Initialize result structure with zero
+res.biterrors   = zeros(conf.nframes,1);
+res.rxnbits     = zeros(conf.nframes,1);
+
+
+for k=1:conf.nframes
+    
+    % Generate random data
+    txbits = randi([0 1],conf.nbits,1);
+    
+    % TODO: Implement tx() Transmit Function
+    [txsignal conf] = txtest(txbits,conf,k);
+    
+    % % % % % % % % % % % %
+    % Begin
+    % Audio Transmission
+    %
+    
+    % ???? normalize values 
+    peakvalue       = max(abs(txsignal));
+    normtxsignal    = txsignal / peakvalue;
+    % normalize the peak to 1
+    
+    % create vector for transmission
+    % add padding before and after the signal
+    rawtxsignal = [zeros(conf.f_sampling,1); normtxsignal; zeros(conf.f_sampling,1)]; 
+    % add second channel: no signal
+    rawtxsignal = [zeros(size(rawtxsignal)), rawtxsignal]; 
+    % calculate length of transmitted signal (in second)
+    txdur = length(rawtxsignal)/conf.f_sampling; 
+    
+%     wavwrite(rawtxsignal,conf.f_s,16,'out.wav')   
+    audiowrite('out.wav',rawtxsignal,conf.f_sampling)  
+    
+    % Platform native audio mode 
+    if strcmp(conf.audiosystem,'native')
+        
+        % Windows WAV mode 
+        if ispc()
+            disp('Windows WAV');
+            wavplay(rawtxsignal,conf.f_sampling,'async');
+            disp('Recording in Progress');
+            rawrxsignal = wavrecord((txdur+1)*conf.f_sampling,conf.f_sampling);
+            disp('Recording complete')
+            rxsignal = rawrxsignal(1:end,1);
+
+        % ALSA WAV mode 
+        elseif isunix()
+            disp('Linux ALSA');
+            cmd = sprintf('arecord -c 2 -r %d -f s16_le  -d %d in.wav &',conf.f_sampling,ceil(txdur)+1);
+            system(cmd); 
+            disp('Recording in Progress');
+            system('aplay  out.wav')
+            pause(2);
+            disp('Recording complete')
+            rawrxsignal = wavread('in.wav');
+            rxsignal    = rawrxsignal(1:end,1);
+        end
+        
+    % MATLAB audio mode
+    elseif strcmp(conf.audiosystem,'matlab')
+        disp('MATLAB generic');
+        playobj = audioplayer(rawtxsignal,conf.f_sampling,conf.bitsps);
+        recobj  = audiorecorder(conf.f_sampling,conf.bitsps,1);
+        record(recobj);
+        disp('Recording in Progress');
+        playblocking(playobj)
+        pause(0.5);
+        stop(recobj);
+        disp('Recording complete')
+        rawrxsignal  = getaudiodata(recobj,'int16');
+        rxsignal     = double(rawrxsignal(1:end))/double(intmax('int16')) ;
+        
+    elseif strcmp(conf.audiosystem,'bypass')
+        rawrxsignal = rawtxsignal(:,1);
+        rxsignal    = rawrxsignal;
+    end
+    
+    % Plot received signal for debgging
+    figure(4);
+    plot(rawtxsignal(:, 2)); hold on;
+    plot(rxsignal); hold off;
+    axis([1, length(rxsignal), -0.7, 0.7])
+    legend('rawtxsignal', 'rxsignal')
+    title('Received Signal')
+    
+    %
+    % End
+    % Audio Transmission   
+    % % % % % % % % % % % %
+    
+    % TODO: Implement rx() Receive Function
+    % [rxbits conf]       = rx(rxsignal,conf);
+    [rxbits conf]       = rxtest(rxsignal, rawtxsignal(:, 2),conf);
+    
+    res.rxnbits(k)      = length(rxbits);  
+    res.biterrors(k)    = sum(rxbits ~= txbits);
+    
+    pause(3);
+    
+end
+
+per = sum(res.biterrors > 0)/conf.nframes
+ber = sum(res.biterrors)/sum(res.rxnbits)
---
+++ txtest.m
@@ -0,0 +1,88 @@
+function [txsignal conf] = txtest(txbits, conf, frame_ind)
+
+modulation_order    = conf.modulation_order;
+os_factor           = conf.os_factor;
+nsubcarriers        = conf.nsubcarriers;
+npreamble           = conf.npreamble;
+nsyms               = conf.nsyms;
+cp_factor           = conf.cp_factor;
+f_sampling          = conf.f_sampling;
+f_mixer             = conf.f_mixer;
+
+%% Genetate & pulse-shape preamble
+preamble_bits = preamble_generate(npreamble);
+% BPSK
+preamble_sym = modulator(preamble_bits, 1);
+% upsampling preamble
+% preamble_sym = upsample(preamble_sym, os_factor);
+preamble_sym = reshape(repmat(preamble_sym.', os_factor, 1), [], 1);
+% pulse shaping 
+preamble_sym = conv(preamble_sym, rrc(os_factor), 'same');
+% % Visualize the preamble
+% figure(1);
+% stem(preamble_sym(1:200), 'Linewidth', 1); 
+% title('example of preamble after pulse shaping');
+
+%% Generate training symbol with preamble
+train_sym = train_sym_generate(npreamble, nsubcarriers, 1);
+
+%% fill the empty space of the last block
+if (mod(nsyms, nsubcarriers)~=0)
+   txbits_fill = ...
+       [txbits; randi([0, 1], (nsubcarriers-mod(nsyms, nsubcarriers))*2, 1)]; 
+end
+
+%% QPSK modulation
+txsyms_ori = modulator(txbits_fill, modulation_order);
+
+%% Reshape to size of (nsubcarriers, []) and add training symbol
+txsyms_reshape = [train_sym, reshape(txsyms_ori, nsubcarriers, [])];
+
+%% IFFT
+txsyms_ifft = osifft_mod(txsyms_reshape, os_factor);
+
+%% Add cyclic prefix
+txsyms_with_cp = ...
+    [txsyms_ifft((end-ceil(size(txsyms_ifft, 1)*cp_factor)+1):end, : ); txsyms_ifft];
+% % Visualize the signal after ifft
+% figure(2);
+% subplot(211);
+% plot(real(txsyms_with_cp( : , 2)), 'Linewidth', 1); hold on;
+% line(nsubcarriers*os_factor/2*ones(1, 2), 0.06*[-1, 1], ...
+%     'Color', 'm', 'LineStyle', '--', 'LineWidth', 2); 
+% line(nsubcarriers*os_factor*ones(1, 2), 0.06*[-1, 1], ...
+%     'Color', 'm', 'LineStyle', '--', 'LineWidth', 2);
+% hold off;
+% xlabel('index'); ylabel('real part'); title('Real part of an OFDM symbol');
+% subplot(212);
+% plot(imag(txsyms_with_cp( : , 2)), 'r', 'Linewidth', 1); hold on;
+% line(nsubcarriers*os_factor/2*ones(1, 2), 0.045*[-1, 1], ...
+%     'Color', '#4DBEEE', 'LineStyle', '--', 'LineWidth', 2); 
+% line(nsubcarriers*os_factor*ones(1, 2), 0.045*[-1, 1], ...
+%     'Color', '#4DBEEE', 'LineStyle', '--', 'LineWidth', 2);
+% hold off;
+% xlabel('index'); ylabel('imaginary part'); title('Imaginary part of an OFDM symble');
+
+%% Assemble to signal to transmit
+% 
+txsyms_ready = [preamble_sym; ...
+    mean(abs(preamble_sym), 'all')/mean(abs(txsyms_with_cp), 'all')/2*reshape(txsyms_with_cp, [], 1)];
+
+% txsyms_ready = [preamble_sym; ...
+%     max(abs(preamble_sym), [], 'all')/max(abs(txsyms_with_cp), [], 'all')*reshape(txsyms_with_cp, [], 1)];
+
+
+%% Carrier_sequence
+carrier_seq = exp(1i*2*pi*(f_mixer/f_sampling)*(0:length(txsyms_ready)-1));
+txsignal = real(txsyms_ready.*(carrier_seq.'));
+% % Visualize the transmitted signal
+% figure(3);
+% subplot(211);
+% plot(txsignal(1:nsubcarriers), 'Linewidth', 1); 
+% xlabel('1:N'); title('First N samples of txsignal');
+% subplot(212);
+% plot(txsignal(end-nsubcarriers+1:end), 'r', 'Linewidth', 1); 
+% xlabel('Last N'); title('Last N samples of txsignal');
+
+end
+
